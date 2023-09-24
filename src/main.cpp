ccf786360eca3859f34cdd05b3a404266029e6eb
cornelcristea <petrucornel.cristea@gmail.com>
2023-09-24 17:23:12 +0300

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,19 @@ using namespace std;
 
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
 #include "lib/clib1/clib1.h"
 #include "lib/clib2/clib2.h"
 
 int main(){
-    CLib1::printHelloWorld();
-    CLib2::printBazelMessage();
+    int index = 1;
+    while (index > 0){
+        auto wakeupTime = chrono::system_clock::now() + chrono::seconds(5);
+        this_thread::sleep_until(wakeupTime);
+        CLib1::printHelloWorld();
+        CLib2::printBazelMessage();
+        cout << endl;
+    }
     return 0;
 }
